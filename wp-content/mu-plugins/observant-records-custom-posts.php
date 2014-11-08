105ecf61dcb1c3f0d11eb4a42f41d593a15d4616
Greg Bueno <greg@gregbueno.com>
2014-11-08 08:15:21 -0800

    Adding custom plugins to mu-plugins.

---
+++ wp-content/mu-plugins/observant-records-custom-posts.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gbueno
+ * Date: 10/14/2014
+ * Time: 12:44 PM
+ */
+
+require WPMU_PLUGIN_DIR.'/observant-records-custom-posts/observant-records-custom-posts.php';
