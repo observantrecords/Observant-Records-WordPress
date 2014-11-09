8e08da275d4ec0ea8338d53c808e7b8bb005c2e4
Greg Bueno <greg@gregbueno.com>
2014-11-09 00:01:30 -0800

    Removing custom posts from mu-plugins.

--- wp-content/mu-plugins/observant-records-custom-posts.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: gbueno
- * Date: 10/14/2014
- * Time: 12:44 PM
- */
-
-require WPMU_PLUGIN_DIR.'/observant-records-custom-posts/observant-records-custom-posts.php';
